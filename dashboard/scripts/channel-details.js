de74582a839b2405a40efe0f595a7861123998ce
dani quinn sexton <dani@imakenicethin.gs>
2017-04-30 00:04:56 +0100

    Fix stream timers and add feedback when channel ID is invalid

--- dashboard/scripts/channel-details.js
+++ dashboard/scripts/channel-details.js
@@ -4,12 +4,21 @@
   const channelInfo = nodecg.Replicant('channel.info', 'nodecg-twitch-service')
   const streamInfo = nodecg.Replicant('stream.info', 'nodecg-twitch-service')
   const loggedInStatus = nodecg.Replicant('login.status', 'nodecg-twitch-service')
+  const userInfo = nodecg.Replicant('user.info', 'nodecg-twitch-service')
 
   let streamStartedAt
 
   const elements = {
-    loginMessage: document.getElementById('login-message'),
-    channelInfo: document.getElementById('channel-info'),
+    sections: {
+      login: document.getElementById('login-section'),
+      channel: document.getElementById('channel-section'),
+    },
+
+    panels: {
+      error: document.getElementById('error-panel'),
+      info: document.getElementById('info-panel'),
+    },
+
     loading: document.getElementById('loading'),
     stats: document.getElementById('stats'),
     viewers: document.getElementById('stat.viewers'),
@@ -20,8 +29,8 @@
   loggedInStatus.on(
     'change',
     (isLoggedIn) => {
-      elements.loginMessage.style.display = isLoggedIn ? 'none' : 'block'
-      elements.channelInfo.style.display = isLoggedIn ? 'block' : 'none'
+      elements.sections.login.style.display = isLoggedIn ? 'none' : 'block'
+      elements.sections.channel.style.display = isLoggedIn ? 'block' : 'none'
     }
   )
 
@@ -40,6 +49,14 @@
 
   setInterval(tick, 1 * 1000)
 
+  userInfo.on(
+    'change',
+    ({ unknown = false } = {}) => {
+      elements.panels.error.style.display = unknown ? 'block' : 'none'
+      elements.panels.info.style.display = unknown ? 'none' : 'block'
+    }
+  )
+
   channelInfo.on(
     'change',
     (channel) => {
@@ -57,9 +74,9 @@
 
   streamInfo.on(
     'change',
-    ({ viewers = 0, timer = 0 } = {}) => {
+    ({ viewers = 0, created_at: createdAt } = {}) => {
       elements.viewers.innerHTML = viewers
-      streamStartedAt = timer ? moment(timer) : undefined
+      streamStartedAt = createdAt ? moment(createdAt) : undefined
       tick()
     }
   )
