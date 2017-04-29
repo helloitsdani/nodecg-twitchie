39797e8a5c6558021fa048750add878cbd82a7a0
dani quinn sexton <dani@imakenicethin.gs>
2017-04-29 19:11:05 +0100

    Refactor extension structure for (hopefully) better clarity

---
+++ extension/service.js
@@ -0,0 +1,112 @@
+const express = require('express')
+const login = require('../../../lib/login')
+
+const twitchModule = require('./twitch')
+const chatModule = require('./chat')
+const channelModule = require('./channel')
+
+const isTwitchSession = session => (
+  session.passport
+  && session.passport.user
+  && session.passport.user.provider === 'twitch'
+)
+
+const hasAccessDetails = session => (
+  session.passport.user.username
+  && session.passport.user.accessToken
+)
+
+
+module.exports = (nodecg) => {
+  const app = express()
+  let twitch
+
+  const performConnect = (session) => {
+    const { user } = session.passport
+
+    // twitch currently doesn't expire oauth tokens
+    // therefore i'm going to naively assume that once
+    // we've logged in and connected, it should be okay
+    // for the lifespan of the server/stream
+    twitch = twitchModule(nodecg, {
+      username: user.username,
+      token: user.accessToken
+    })
+
+    twitch.connect()
+      .then(() => {
+        chatModule(nodecg, twitch)
+        channelModule(nodecg, twitch)
+      })
+  }
+
+  const performDisconnect = () => {
+    twitch.disconnect()
+      .then(() => {
+        twitch = undefined
+      })
+      .catch(() => {
+        twitch = undefined
+      })
+  }
+
+  const handleConnect = (session) => {
+    if (!isTwitchSession(session) || !hasAccessDetails(session)) {
+      throw new Error('Invalid session data receieved')
+    }
+
+    if (twitch) {
+      performDisconnect()
+    }
+
+    performConnect(session)
+  }
+
+  const handleDisconnect = () => {
+    if (!twitch) {
+      return
+    }
+
+    performDisconnect()
+  }
+
+
+  // listen for login and logout events emitted from nodecg's login module
+  login.on('login', handleConnect)
+  login.on('logout', handleDisconnect)
+
+  // create an express subapp with a route to refresh our the backend's twitch
+  // info based on the user's login stateh
+  // hitting this periodically from the dashboard allows us to keep in sync
+  // even if the user hasn't performed a login this browser/server session
+  app.get(
+    '/login/twitch/verify',
+    (request, response) => {
+      const session = request.session
+
+      nodecg.log.debug('Token refresh endpoint hit...')
+
+      if (!twitch) {
+        try {
+          handleConnect(session)
+          nodecg.log.debug('Connected to Twitch.')
+        } catch (e) {
+          nodecg.log.error('Could not connect to Twitch!', e)
+          return response.sendStatus(401)
+        }
+      } else {
+        nodecg.log.debug('No update necessary.')
+      }
+
+      return response.sendStatus(200)
+    }
+  )
+
+  // mount our refresh route under the main nodecg express app
+  nodecg.mount(app)
+
+  // exposes a reference to the current twitch client for other modules to consume
+  return {
+    twitch,
+  }
+}
