28055f40a443fce9b3c7e2cce712631798f6ea6a
dani quinn sexton <dani@imakenicethin.gs>
2017-05-27 01:28:34 +0100

    Add test stubs for message parsing utils

---
+++ extension/utils/__tests__/parseMessage.js
@@ -0,0 +1,45 @@
+import test from 'ava'
+
+test('tokeniseMessage: handles invalid parameters', (t) => {
+  t.fail()
+})
+
+test('tokeniseMessage: creates tokens for each provided instance', (t) => {
+  t.fail()
+})
+
+test('tokeniseMessage: passes content and type through to tokens correctly', (t) => {
+  t.fail()
+})
+
+test('tokeniseMessage: creates text tokens inbetween instances', (t) => {
+  t.fail()
+})
+
+test('tokeniseMessage: creates text tokens from trailing strings', (t) => {
+  t.fail()
+})
+
+test('sortTokens: orders tokens correctly', (t) => {
+  t.fail()
+})
+
+test('getEmoteTokens: parses twitch token format correctly', (t) => {
+  t.fail()
+})
+
+test('getEmoteTokens: returns an array of tokens sorted in occurrence order', (t) => {
+  t.fail()
+})
+
+test('getCheermoteTokens: parses out cheermote instances correctly', (t) => {
+  t.fail()
+})
+
+test('getCheermoteTokens: returns an array of tokens sorted in occurrence order', (t) => {
+  t.fail()
+})
+
+test('parseTokens: only tokenises text tokens', (t) => {
+  t.fail()
+})
