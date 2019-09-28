ee7e23547e8641978b6eb00628f468866d5f521e
dani quinn sexton <spells@witch.zone>
2019-09-28 18:23:10 +0100

    Remove built assets

--- src/common/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const NAMESPACE = 'nodecg-twitchie';
-exports.NAMESPACE = NAMESPACE;
-//# sourceMappingURL=constants.js.map
