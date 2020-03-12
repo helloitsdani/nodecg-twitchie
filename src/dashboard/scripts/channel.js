6dfda736cf72afbcc47bffeb6815c1eba91c7927
dani quinn sexton <spells@witch.zone>
2020-03-12 02:29:03 +0000

    Replace parcel with webpack for building panels

---
+++ src/dashboard/scripts/channel.js
@@ -0,0 +1,6 @@
+import '../components/twitchie-channel-info/twitchie-channel-info.js'
+import './verify-login'
+
+document.getElementById('app').innerHTML = `
+  <twitchie-channel-info></twitchie-channel-info>
+`
