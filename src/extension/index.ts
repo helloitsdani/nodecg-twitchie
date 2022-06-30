2ed1037a16da20dec2ad5223598384c8e9844e8a
dani quinn sexton <dani@glitter.tech>
2022-06-30 22:01:29 +0100

    Export specific interface instead of proxy

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,3 +1,5 @@
+import { TwitchieExtension } from '../types'
+
 import { TwitchieClientWrapper } from './client'
 import context from './context'
 
@@ -21,25 +23,23 @@ module.exports = async (nodecg: any) => {
   require('./modules/channel')
   require('./modules/chat')
 
-  // exposes the module's EventEmitter, and proxies requests to the current
-  // twitch instance if one exists--we can't just return the twitch object,
-  // because we create and destroy it based on user login status
-  // this lets other nodecg bundles access the twitch api and recieve events we emit
-  return new Proxy(context.events, {
-    get: (target, method) => {
-      if (method === 'replicants') {
-        return context.replicants
-      }
-
-      if (method === 'api') {
-        return context.twitch.api
-      }
-
-      if (method === 'client') {
-        return context.twitch.client
-      }
-
-      return (target as any)[method]
+  const twitchieExtension: TwitchieExtension = {
+    get replicants() {
+      return context.replicants
     },
-  })
+
+    get api() {
+      return context.twitch.api
+    },
+
+    get client() {
+      return context.twitch.client
+    },
+
+    on(name, handler) {
+      return context.events.addListener(name, handler)
+    },
+  }
+
+  return twitchieExtension
 }
